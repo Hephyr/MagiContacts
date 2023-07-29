7e3c441f6a760c89fc99122fc7d51529b6052953
Hefeng Xu <ixuhefeng@gmail.com>
2023-07-29 09:41:50 +0800

    Initial Commit

---
+++ MagiContacts/MagiContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagiContactsApp.swift
+//  MagiContacts
+//
+//  Created by Hefeng Xu on 7/29/23.
+//
+
+import SwiftUI
+
+@main
+struct MagiContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
